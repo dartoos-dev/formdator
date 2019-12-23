8cbbfacc3fb9c72c972fc722d73dbbebe41f3556
Rafael Amizes <r.amizes@gmail.com>
2019-12-23 19:13:41 -0300

    Add MaxLength validator

--- lib/callor.dart
+++ lib/callor.dart
@@ -4,3 +4,4 @@ export 'src/required.dart';
 export 'src/rules.dart';
 export 'src/email.dart';
 export 'src/min_length.dart';
+export 'src/max_length.dart';
---
+++ lib/src/max_length.dart
@@ -0,0 +1,19 @@
+/// Maximum length rule.
+class MaxLength {
+
+  // The maximum length value.
+  final int _len;
+  // Error message.
+  final String _msg;
+
+  const MaxLength(int length, {msg='Maximum length exceeded.'}) :
+    _len = length,
+    _msg =  msg; 
+
+  /// Checks whether the given value has not exceeded the maximun
+  /// number of characteres.
+  String call(String value) {
+    return value.length > _len ? _msg : null;
+  }
+}
+
