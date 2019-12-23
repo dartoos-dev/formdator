79e19cbfa8bc221af92ff4ca1c0e0ed2cd4794cf
Rafael Amizes <r.amizes@gmail.com>
2019-12-23 18:59:55 -0300

    Add email validator

--- lib/callor.dart
+++ lib/callor.dart
@@ -2,3 +2,4 @@ library callor;
 
 export 'src/required.dart';
 export 'src/rules.dart';
+export 'src/email.dart';
---
+++ lib/src/email.dart
@@ -0,0 +1,15 @@
+import 'package:validators/validators.dart';
+
+/// Well formed email rule.
+class Email {
+
+  final String _msg;
+
+  const Email({String msg='Invalid email.'}) :
+    _msg = msg;
+
+  /// Checks whether the given value is a well formed email.
+  String call(String value) {
+    return isEmail(value)? null : _msg;
+  }
+}
