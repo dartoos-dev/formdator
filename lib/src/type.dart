80203b5764d160b96360dac55779042e2275a38c
Rafael Amizes <r.amizes@gmail.com>
2019-12-23 18:42:18 -0300

    Add parameter to Val type Function

--- lib/src/type.dart
+++ lib/src/type.dart
@@ -1,2 +1,6 @@
-/// Represents a callable validator.
-typedef Val = String Function();
+/// Represents a callable Flutter form field validator.
+
+/// According to the Flutter form standards, a field validator 
+/// must return a descriptive String message to indicate that the
+/// given value is valid; null otherwise.
+typedef Val = String Function(String value);
