7d71c3b33b68a54dcf9ded1ad1cd4a4830ced28b
Rafael Amizes <r.amizes@gmail.com>
2019-12-23 20:01:50 -0300

    Reformat Equal validator code

--- lib/src/equal.dart
+++ lib/src/equal.dart
@@ -3,22 +3,23 @@ typedef Original = String Function();
 
 /// Equality rule.
 class Equal {
-  // The original value to be compared to.
+  // Something that retrieves the original value to be compared to.
   final Original _original;
   // Error message.
   final String _msg;
 
-  static const _defaultMsg = 'Does not match';
+  static const _defaultMsg = 'Does not match.';
 
-  Equal(Object obj, {msg: _defaultMsg}) :
-    this.value(obj.toString, msg: msg);
+  Equal(Object obj, {msg = _defaultMsg}) :
+    this.origin(obj.toString, msg: msg);
 
-  Equal.value(Original original, {msg = _defaultMsg})
-    : _original = original, _msg = msg;
+  Equal.origin(Original original, {msg = _defaultMsg}) :
+    _original = original,
+    _msg = msg;
 
   ///Checks whether the given value is equal to the original one.
   String call(String value) {
-    return value != _original()? _msg : null;
+    return value == _original()? null : _msg;
   }
 }
 
