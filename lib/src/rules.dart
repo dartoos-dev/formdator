e83b3454894a6804a6de837b7322c4146204698e
Rafael Amizes <r.amizes@gmail.com>
2020-01-04 18:59:57 -0300

    Add 2 rules special case constructor
    The convenience 2 rules constructor was added to Rules class so that
    special case 2 rules classes could be easily implemented.

--- lib/src/rules.dart
+++ lib/src/rules.dart
@@ -1,9 +1,17 @@
 import 'type.dart';
-
+import 'required.dart';
 /// Represents multiple rules applied to an text field.
 class Rules {
 
   final List<Val> _rules;
+
+  ///Special case constructor.
+  ///It is useful when two rules must be applied and the first one
+  ///is the required filled. (a field that cannot be null or blank)
+  ///[blank] is the error message for a not filled in field.
+  ///[val] is the second validator.
+  Rules.req(String blank, Val val) :
+    this([Required(msg: blank), val]);
 
   const Rules(this._rules);
 
