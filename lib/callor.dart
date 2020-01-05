1a6aba236919062822e2925a0a56488012dab6d2
Rafael Amizes <r.amizes@gmail.com>
2020-01-04 21:01:08 -0300

    Export trimmed required validators

--- lib/callor.dart
+++ lib/callor.dart
@@ -1,15 +1,21 @@
 library callor;
 
-export 'src/required.dart';
-export 'src/rules.dart';
 export 'src/email.dart';
-export 'src/req_email.dart';
+export 'src/cpf.dart';
+export 'src/cnpj.dart';
 export 'src/min_length.dart';
 export 'src/max_length.dart';
 export 'src/numeric.dart';
 export 'src/equal.dart';
-export 'src/cpf.dart';
+export 'src/rules.dart';
+export 'src/required.dart';
+export 'src/req_email.dart';
 export 'src/req_cpf.dart';
-export 'src/cnpj.dart';
 export 'src/req_cnpj.dart';
 export 'src/trimmed.dart';
+export 'src/trim_req.dart';
+export 'src/trim_rules.dart';
+export 'src/trim_rules.dart';
+export 'src/trim_req_email.dart';
+export 'src/trim_req_cpf.dart';
+export 'src/trim_req_cnpj.dart';
