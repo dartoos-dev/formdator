8844d195aa505f3c18ebd2b570f35d26e3a341ba
Rafael Amizes <r.amizes@gmail.com>
2020-01-22 14:31:56 -0300

    Create required datetime validator

---
+++ lib/src/req_dt.dart
@@ -0,0 +1,9 @@
+/// Required datetime.
+class ReqDt{
+
+  final String _msg;
+
+  const ReqDt({String blank = 'Required.'}) : _msg = blank;
+
+  String call(DateTime dt) => dt == null? _msg : null;
+}
