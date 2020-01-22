5df8447d03cc52dbdd640ef9cf2eb72d837c9288
Rafael Amizes <r.amizes@gmail.com>
2020-01-22 14:32:15 -0300

    Create wellformed datetime validator

---
+++ lib/src/wellformed_dt.dart
@@ -0,0 +1,18 @@
+/// Well formed datetime validator.
+class WellFormedDt {
+
+  final String _msg;
+
+  const WellFormedDt({String malformed='Malformed value.'})
+    : _msg = malformed;
+
+  String call(DateTime dt) {
+    String ok;
+    try {
+      DateTime.parse(dt.toString());
+    } catch (e) {
+      ok = _msg;
+    }
+    return ok;
+  }
+}
