416af5ed67caa9ccdaa3ef6ab42de470daf076fc
Rafael Amizes <r.amizes@gmail.com>
2020-01-22 14:32:46 -0300

    Create multiple rules datetime validator

---
+++ lib/src/rules_dt.dart
@@ -0,0 +1,18 @@
+import 'type.dart';
+
+///Multiple datetime validator.
+class RulesDt {
+
+  final List<DtValidator> _vals;
+
+  const RulesDt(this._vals);
+
+  String call(DateTime dt) {
+    String msg;
+    for (final val in _vals) {
+      msg = val(dt);
+      if (msg != null) break;
+    }
+    return msg;
+  }
+}
