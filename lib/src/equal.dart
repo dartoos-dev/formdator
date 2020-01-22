e2117045fa7d78e4246750c1fe5a77357d50dcf7
Rafael Amizes <r.amizes@gmail.com>
2020-01-22 18:30:08 -0300

    Fix and simplify Equal validator
    This is breaking change, because the message string changed from "msg"
    to "diff".

--- lib/src/equal.dart
+++ lib/src/equal.dart
@@ -1,27 +1,16 @@
-/// The original String value.
-typedef Original = String Function();
-
 /// Equality rule.
 class Equal {
-  // Something that retrieves the original value to be compared to.
-  final Original _original;
+  // The original value to be compared to.
+  final String _origin;
   // Error message.
   final String _msg;
 
-  static const _defaultMsg = 'Does not match.';
-
-  /// The original message will be retrieved by calling obj.toString
-  Equal(Object obj, {msg = _defaultMsg}) :
-    this.origin(obj.toString, msg: msg);
-
   /// Main constructor.
-  Equal.origin(Original original, {msg = _defaultMsg}) :
-    _original = original,
-    _msg = msg;
+  Equal(String origin, {String diff = 'Does not match.'})
+    : _origin = origin,
+      _msg = diff;
 
   ///Checks whether the given value is equal to the original one.
-  String call(String value) {
-    return value == _original()? null : _msg;
-  }
+  String call(String value) => value == _origin? null : _msg;
 }
 
