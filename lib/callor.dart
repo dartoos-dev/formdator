dc5234d23f23bdfdaa8ca262292ca8ba489dda51
Rafael Amizes <r.amizes@gmail.com>
2020-01-22 14:44:59 -0300

    Export ok and datetiem related validators

--- lib/callor.dart
+++ lib/callor.dart
@@ -19,3 +19,8 @@ export 'src/trim_rules.dart';
 export 'src/trim_req_email.dart';
 export 'src/trim_req_cpf.dart';
 export 'src/trim_req_cnpj.dart';
+export 'src/ok.dart';
+export 'src/req_dt.dart';
+export 'src/wellformed_dt.dart';
+export 'src/req_wellformed_dt.dart';
+export 'src/rules_dt.dart';
